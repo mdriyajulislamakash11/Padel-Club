62b676d119b5a87601b86aba6af309a7d026af17
akash <hafjmohammadakash@gmail.com>
2025-12-24 21:13:27 -0800

    corsor aimation

--- padel.js
+++ padel.js
@@ -1,31 +1,55 @@
-   
-   var tl = gsap.timeline();
+var tl = gsap.timeline();
 
-   tl.from("li", {
-     y: -50,
-     opacity: 5,
-     duration: 1,
-     delay: 0.2,
-     stagger: 0.3,
-   });
-   
-   
-   
-   gsap.to(".boox", {
- 
-      rotation: 3360,
-      duration: 2.5,
-      repeat: Infinity,
-      yoyo: true,
-scale: 0.2,
-borderradius: "50%",
-    });
+tl.from("li", {
+  y: -50,
+  opacity: 5,
+  duration: 1,
+  delay: 0.2,
+  stagger: 0.3,
+});
 
-    gsap.to(".boox1", {
-      backgroundColor: "#22c55e",
-      rotation: -360,
-      duration: 2,
-      repeat: -1,
-      yoyo: true,
-      scale: 0.2,
-    });
+gsap.from(".header-h1", {
+  x: 900,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  scrollTrigger: {
+    trigger: ".header-h1",
+    scroller: "body",
+  },
+});
+
+gsap.from(".header-p", {
+  x: -900,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+});
+
+// Animate card from Left
+gsap.from(".left-animate", {
+  x: -800,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  stagger: 0.3,
+  scrollTrigger: {
+    trigger: ".left-animate",
+    scroller: "body",
+  },
+});
+
+// Custom Cursor
+const cursor = document.querySelector("#cursor");
+const main = document.querySelector("#main");
+
+main.addEventListener("mousemove", (e) => {
+  gsap.to(cursor, {
+    x: e.clientX - cursor.offsetWidth / 2,
+    y: e.clientY - cursor.offsetHeight / 2,
+    duration: 0.3,
+    ease: "Power3.easeOut",
+    
+  });
+});
+
